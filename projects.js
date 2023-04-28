c1a9d5801b136814c00bc732770811b9296be5fa
Clembs <clembs@clembs.com>
2023-04-28 19:31:41 +0200

    add project array file

---
+++ projects.js
@@ -0,0 +1,47 @@
+const projects = [
+  {
+    url: 'https://cdn.discordapp.com/attachments/986331940967841792/1101556660113260544/a_cult_final_render__signature.png',
+    thumb:
+      './assets/projects/thumbnails/a cult final render + signature-small.png',
+  },
+  {
+    url: 'https://cdn.discordapp.com/attachments/986331940967841792/1101556693869015220/b_TENTEEE_jour_final.png',
+    thumb: './assets/projects/thumbnails/b TENTEEE jour final-small.png',
+  },
+  {
+    url: 'https://www.youtube.com/embed/yv1sXfpf8fA',
+    thumb: './assets/projects/thumbnails/FEFE40F-small.png',
+  },
+  {
+    url: 'https://cdn.discordapp.com/attachments/986331940967841792/1101556700072382524/floralis_final-min.png',
+    thumb: './assets/projects/thumbnails/floralis final-min-small.png',
+  },
+  {
+    url: 'https://cdn.discordapp.com/attachments/986331940967841792/1101556711749341214/floralis_sunset-min.png',
+    thumb: './assets/projects/thumbnails/floralis sunset-min-small.png',
+  },
+  {
+    url: 'https://cdn.discordapp.com/attachments/986331940967841792/1101556722214117377/gun_pink_style_final-min.png',
+    thumb: './assets/projects/thumbnails/gun pink stylé final-min-small.png',
+  },
+  {
+    url: 'https://cdn.discordapp.com/attachments/986331940967841792/1101556738546749510/maskchrome_edit.png',
+    thumb: './assets/projects/thumbnails/maskchrome edit-small.png',
+  },
+  {
+    url: 'https://cdn.discordapp.com/attachments/986331940967841792/1101547338637512724/porsche_shot_5_ou_6.png',
+    thumb: './assets/projects/thumbnails/porsche_shot_5_ou_6-small.png',
+  },
+  {
+    url: 'https://cdn.discordapp.com/attachments/986331940967841792/1101556749070250014/poster_komori-min.png',
+    thumb: './assets/projects/thumbnails/poster komori-min-small.png',
+  },
+  {
+    url: 'https://www.youtube.com/embed/4_8ymsPQuNg',
+    thumb: './assets/projects/thumbnails/speaker ad final-small.png',
+  },
+  {
+    url: 'https://www.youtube.com/embed/qFShz-yGLps',
+    thumb: './assets/projects/thumbnails/staring-small.png',
+  },
+];
